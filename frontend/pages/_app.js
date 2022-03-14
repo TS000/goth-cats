f1b2572cff29af8103caccc28289c2b614051bbc
TS000 <tylersnell@me.com>
2022-03-14 12:13:11 -0700

    babel fix

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
