6b20849aa962962d535e61fb363be85a7a543571
TS000 <tylersnell@me.com>
2022-03-14 10:49:40 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goth-cats
+This is a graphQL API used to query gifs of gothic felines.
+
+![Alt Text](https://media.giphy.com/media/LmgHHxtKgDsYrVsEOw/giphy.gif)
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Gifs {
+    title: String
+    link: String
+  }
+
+  type Query {
+    gifs: [Gifs]
+  }
+`;
+
+const gifs = [
+  {
+    title: "Creepy Cat",
+    link: "https://i.giphy.com/media/BJa0a20ni8yYHrQXfl/giphy.gif",
+  },
+  {
+    title: "Dark Cat",
+    link: "https://i.giphy.com/media/PGGsOfnjKBHvW/giphy.gif",
+  },
+  {
+    title: "Angry Cat",
+    link: "https://i.giphy.com/media/PGGsOfnjKBHvW/giphy.gif",
+  },
+  {
+    title: "Scared Cat",
+    link: "https://i.giphy.com/media/PGGsOfnjKBHvW/giphy.gif",
+  },
+  {
+    title: "Judgy Cat",
+    link: "https://i.giphy.com/media/PGGsOfnjKBHvW/giphy.gif",
+  },
+];
+
+const resolvers = {
+  Query: {
+    gifs: () => gifs,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
